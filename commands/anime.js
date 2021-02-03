ab43cb38b9f818292ea9fdc86efb1478924bda3b
djpie123 <shreyashvijay1710@gmail.com>
2021-02-03 10:23:26 +0530

    new

--- commands/anime.js
+++ commands/anime.js
@@ -1,49 +1,33 @@
-//By NightcoreAT#3678
-const request = require('node-superfetch'); //npm i node-superfetch 
-const Discord = require('discord.js'); //npm i discord.js
-//no footer so we are not required to use customisation
-//Please note that the search does not support multiple languages
-exports.run = async (client, message, args, prefix) => { //From here our command starts
-    //name: 'kitsu-anime',
-    //aliases: ['kanime', 'anime', 'kitsu-a'],
-    //description: 'provides info about an anime',
-    //usage: '[Anime Name]',
-    //async execute(client, kayn, REGIONS, config, message, args, con, guilds) {
-    function shorten(text, maxLen = 2000) { //Let’s tell the bot that the maximum number of characters is 2000
-		return text.length > maxLen ? `${text.substr(0, maxLen - 3)}...` : text;
-	}
-    const query = args.join(' '); //After entering the name
-    try {
-		const { text } = await request //From here, the bot will start searching for your request 
-			.get('https://kitsu.io/api/edge/anime') //To check the bot from kitsu.io api 
-			.query({ 'filter[text]': query }); //The bot starts collecting the search results
-		const body = JSON.parse(text); //after done let's check 
-		if (!body.data.length) return message.reply('Could not find any results.'); //If your search is missing or wrong, it does not support most languages
-		const data = body.data[0].attributes; //Let's extract the data
-		const embed = new Discord.RichEmbed() //Let's see the search results
-			.setColor(0xF75239) //optional ; you can set it random  
-			.setAuthor('Kitsu.io', 'https://i.imgur.com/lVqooyd.png', 'https://kitsu.io/explore/anime')
-			.setURL(`https://kitsu.io/anime/${data.slug}`) //let's show your search results from data
-			.setThumbnail(data.posterImage ? data.posterImage.original : null) //your anime image 
-			.setTitle(data.canonicalTitle)
-			.setDescription(shorten(data.synopsis)) //Here it will give you anime description
-			.addField('❯ Type', `${data.showType} - ${data.status}`, true) //To tell you its type, manga, movie or anime; With his statuts if it is continuous or finished
-			.addField('❯ Episodes', data.episodeCount || '???', true) //To tell you how many episodes it has, it may not tell you the number of episodes of most of the episodes if their episodes are not specified in total
-			.addField('❯ Start Date', data.startDate ? new Date(data.startDate).toDateString() : '???', true) //When did it started
-			.addField('❯ End Date', data.endDate ? new Date(data.endDate).toDateString() : '???', true); //When did it ended
-		return message.channel.send(embed);
-	} catch (err) {
-		return message.reply(`ops, something is wrong: \`${err.message}\`. please try again later !`); //Let's check if your search has a bug
-	}
+const Discord = require('discord.js')
+const helpanimae = new Discord.MessageEmbed()
+.setTitle('HELP ANIMAE')
+.setURL('https://discord.gg/HfkfcMS')
+.setAuthor('Terodactyl#6714', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB5OI1Ao0e2dJmvAhXcE_A9tMj14ittIq_wQ&usqp=CAU' )
+.addField('*fight', 'to fight someone', true)
+.addField('*feed', 'to feed someone', true)
+.addField('*hammer', 'to hammer someone', true)
+.addField('*hug', 'to hug someone', true)
+.addField('*kiss', 'to kiss someone', true)
+.addField('*baka', 'to get a baka gif', true)
+.addField('*neko', 'to get a neko', true)
+.addField('*owoify', 'to make the bot say something', true)
+.addField('*poke', 'to poke a person', true)
+.addField('*roll', 'to roll a die', true)
+.addField('*slap ', 'to slap someone', true)
+.addField('*spank', 'to spank someone', true)
+.addField('*smug', "to smug", true)
+.addField('*smack', 'to smack someone', true)
+.addField('*tickle', 'to tickle someone', true)
+.addField('*waifu', 'to get a waifu', true)
+.addField('*invite', 'to add it to your server', true)
+.addField('HOW CAN I REPORT BUGS??', 'JUST DM PIE_IS_LIVE#9325 ABOUT IT', true)
+.setFooter('THIS BOT IS MADE BY PIE IS LIVE 💕|| ')
+.setTimestamp()
+.setColor('RANDOM');
+
+module.exports.run = (client, message, args) => {
+ return message.channel.send(helpanimae);  
+}
+module.exports.help = {
+    name: "help"
 };
-exports.conf = {
-    enabled: true,
-    guildOnly: false,
-    aliases: [], //You can add your aliases as you want
-    permLevel: 0
-  };
-  
-  module.exports.help = {
-    name: "animae"
-};
-//By NightcoreAT3678
