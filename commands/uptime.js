68bf4e5eca9d8e2c251537d8512195afcaa0fd73
djpie123 <shreyashvijay1710@gmail.com>
2021-02-03 13:17:05 +0530

    new

--- commands/uptime.js
+++ commands/uptime.js
@@ -2,12 +2,15 @@
 const moment = require('moment');
 require('moment-duration-format');
 exports.run = (client, message) => {
-  const duration = moment.duration(this.client.uptime).format(' D [Days], H [Hours], m [Minutes], s [Seconds]');
+  var milliseconds = parseInt((client.uptime % 1000) / 100), //This field is intended to give the bot the number of hours and timing it needs to know the length of time the bot is running on
+  seconds = parseInt((client.uptime / 1000) % 60),
+  minutes = parseInt((client.uptime / (1000 * 60)) % 60),
+  hours = parseInt((client.uptime / (1000 * 60 * 60)) % 24);
   const embed = new Discord.MessageEmbed()
       .setColor('RANDOM')
       .setAuthor('BOT UPTIME')
       .setThumbnail(`${this.client.user.displayAvatarURL(({ dynamic: true }))}`)
-      .addField("Uptime:", `${duration}`)
+      .addField("Uptime:",+ hours + " **hours, **" + minutes + "** minutes and **" + seconds + "." + milliseconds + "** seconds!")
       .setFooter(`${this.client.user.username}`, `${this.client.user.displayAvatarURL({ dynamic: true })}`)
       .setTimestamp()
   return message.channel.send(embed);
