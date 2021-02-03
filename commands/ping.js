4c9214d413d70d7117b40314702345ee01928235
djpie123 <shreyashvijay1710@gmail.com>
2021-02-03 13:28:55 +0530

    new

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,16 @@
+const Discord = require('discord.js')
 module.exports.run = (client, message, args) => {
-    return message.reply("Pong! "+Math.round(client.ws.ping));
+    const embed = new Discord.MessageEmbed()
+            .setTitle("Pong!")
+            .setDescription(
+                `Websocket Latency: ${
+                this.client.ws.ping ? Math.floor(this.client.ws.ping) : 0
+                }ms\nBot Latency: ${Math.round(
+                    Date.now() - message.createdTimestamp
+                )}ms`
+            )
+            .setColor("#7289DA");
+            return message.channel.send(embed)
 }
 
 module.exports.help = {
