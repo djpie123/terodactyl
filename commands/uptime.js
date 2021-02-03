e142abe902869be1c3400c392e7eb159ba2b1801
djpie123 <shreyashvijay1710@gmail.com>
2021-02-03 13:08:13 +0530

    new

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,23 +1,17 @@
-// By NightcoreAT#3678
-exports.run = (client, message) => { //From here our script will start
-    var milliseconds = parseInt((client.uptime % 1000) / 100), //This field is intended to give the bot the number of hours and timing it needs to know the length of time the bot is running on
-        seconds = parseInt((client.uptime / 1000) % 60),
-        minutes = parseInt((client.uptime / (1000 * 60)) % 60),
-        hours = parseInt((client.uptime / (1000 * 60 * 60)) % 24);
-
-        hours = (hours < 10) ? "0" + hours : hours;
-        minutes = (minutes < 10) ? "0" + minutes : minutes;
-        seconds = (seconds < 10) ? "0" + seconds : seconds;
-
-        message.channel.send(":chart_with_upwards_trend: I've been running for** " + hours + " **hours, **" + minutes + "** minutes and **" + seconds + "." + milliseconds + "** seconds!"); //Here he will tell in full detail how long he spent while working
+const Command = require("../../Base/Command");
+const moment = require('moment');
+require('moment-duration-format');
+exports.run = (client, message) => {
+  const duration = moment.duration(this.client.uptime).format(' D [Days], H [Hours], m [Minutes], s [Seconds]');
+  const embed = new Discord.MessageEmbed()
+      .setColor('RANDOM')
+      .setAuthor('BOT UPTIME')
+      .setThumbnail(`${this.client.user.displayAvatarURL(({ dynamic: true }))}`)
+      .addField("Uptime:", `${duration}`)
+      .setFooter(`${this.client.user.username}`, `${this.client.user.displayAvatarURL({ dynamic: true })}`)
+      .setTimestamp()
+  return message.channel.send(embed);
 }
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0
-};
 
 module.exports.help = {
   name: "uptime"
