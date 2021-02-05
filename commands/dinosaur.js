2318eaf7500a943bd01a7752593d8f173641b993
djpie123 <shreyashvijay1710@gmail.com>
2021-02-05 13:01:37 +0530

    new

--- commands/dinosaur.js
+++ commands/dinosaur.js
@@ -3,7 +3,8 @@ const { MessageEmbed } = require("discord.js");
 module.exports.run = (client, message, args) => {
 number = 13;
         imageNumber = Math.floor (Math.random() * number) + 1;
-        message.channel.send ({files: ["./images/" + imageNumber + ".png"]})
+        message.reply("HERE'S YOUR DINOSAUR")
+        message.channel.send({files: ["./images/" + imageNumber + ".png"]})
     } 
 
 module.exports.help = {
