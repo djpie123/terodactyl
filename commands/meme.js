9fce9a410bfcb47f3a4be07e77798439bc62ce5c
djpie123 <shreyashvijay1710@gmail.com>
2021-02-09 08:54:59 +0530

    new

--- commands/meme.js
+++ commands/meme.js
@@ -1,13 +1,23 @@
 const Discord = require("discord.js")
-module.exports.run = (client, message, args, api) => {
-	api.meme().then(meme => {
-			const memeembed = new Discord.MessageEmbed()
-				.setTitle(`${meme.title}`)
-				.setImage(meme.url)
-				.setColor('RANDOM')
-				.setURL(meme.link);
+const got = require('got');
+module.exports.run = (client, message, args) => {
+	const embed = new Discord.MessageEmbed()
+	got('https://www.reddit.com/r/memes/random/.json').then(response => {
+		let content = JSON.parse(response.body);
+		let permalink = content[0].data.children[0].data.permalink;
+		let memeUrl = `https://reddit.com${permalink}`;
+		let memeImage = content[0].data.children[0].data.url;
+		let memeTitle = content[0].data.children[0].data.title;
+		let memeUpvotes = content[0].data.children[0].data.ups;
+		let memeDownvotes = content[0].data.children[0].data.downs;
+		let memeNumComments = content[0].data.children[0].data.num_comments;
+		embed.setTitle(`${memeTitle}`)
+		embed.setURL(`${memeUrl}`)
+		embed.setImage(memeImage)
+		embed.setColor('RANDOM')
+		embed.setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`)
 
-			return message.channel.send(memeembed)
+			return message.channel.send(embed)
 		.catch(e => {
 			console.error(e);
 			message.channel.send("something went wrong!");
