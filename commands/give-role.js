9ce020ec1b2411c6d2f19bd0343a06748e570493
djpie123 <shreyashvijay1710@gmail.com>
2021-02-12 09:38:19 +0530

    new

--- commands/give-role.js
+++ commands/give-role.js
@@ -9,7 +9,7 @@ module.exports = {
         if (!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(`You do not have MANAGE_ROLES permission`).then(m => m.delete({ timeout: 5000 }));
 
         if (!args[0] || !args[1]) return message.channel.send("Incorrect usage, It's `<username || user id> <role name || id>").then(m => m.delete({ timeout: 5000 }))
-
+                                                                                                                          
         try {
 
              const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
@@ -22,7 +22,7 @@ module.exports = {
              const embed = new MessageEmbed()
                  .setTitle(`Role Name: ${roleName.name}`)
                  .setDescription(`${message.author} has successfully given the role ${roleName} to ${member.user}`)
-                 .setColor('f3f3f3')
+                 .setColor('RANDOM')
                  .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
                  .setFooter(new Date().toLocaleString())
 
---
+++ commands/nuke.js
@@ -0,0 +1,26 @@
+const { MessageEmbed } = require('discord.js')
+const boom = require(".")
+module.exports = {
+    name: "nuke",
+    description: "Nukes a given channel",
+    run: async(client, message, args) => {
+        if(!message.member.hasPermission("ADMINISTRATOR")) {
+            return message.reply("You do not have the perms to use this cmd!")
+        }
+        let reason = args.join(" ") || "No Reason"
+        if(!message.channel.deletable) {
+            return message.reply("This channel cannot be nuked!")
+        }
+        let newchannel = await message.channel.clone()
+        await message.channel.delete()
+        let embed = new MessageEmbed()
+        .setTitle("Channel Nuked")
+        .setDescription(reason)
+        .setImage(`./boom.gif`)
+        .setColor("RANDOM")
+        await newchannel.send(embed)
+    }
+}
+module.exports.help = {
+    name: "nuke"
+};
