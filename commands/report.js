3d0099587489eb9d6873cb876070f8a57d99deb9
djpie123 <shreyashvijay1710@gmail.com>
2021-02-12 14:00:10 +0530

    new

---
+++ commands/report.js
@@ -0,0 +1,17 @@
+exports.run = (client, message) => { 
+    let args = message.content.split(" ").slice(1);
+    message.react("✔️"); 
+    if (args.join(" ") === "@everyone" || args.join(" ") === "@here") return message.channel.send("You ain't making me Ping anyone BOI!"); // Let’s see what’s going on if you want everyone
+   client.users.get("706714852571348993").send(`${message.author.username}#${message.autor.discriminator} has reprted a bug ||args.join(" ")|| `); 
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [], 
+    permLevel: 0
+};
+
+module.exports.help = {
+    name: "report"
+};
