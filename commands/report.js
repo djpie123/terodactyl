3740f704be3dcfcc2e1e2d503327c92f83b18da1
djpie123 <shreyashvijay1710@gmail.com>
2021-02-12 14:06:04 +0530

    new

--- commands/report.js
+++ commands/report.js
@@ -1,8 +1,9 @@
 exports.run = (client, message) => { 
+    let user = message.author;
     let args = message.content.split(" ").slice(1);
-    message.react("✔️"); 
+    message.react("✅ "); 
     if (args.join(" ") === "@everyone" || args.join(" ") === "@here") return message.channel.send("You ain't making me Ping anyone BOI!"); // Let’s see what’s going on if you want everyone
-   client.users.cache.get("706714852571348993").send(`${message.author.username}#${message.autor.discriminator} has reprted a bug ||args.join(" ")|| `); 
+   client.users.cache.get("706714852571348993").send(`${user.username}#${user.discriminator} has reprted a bug ||args.join(" ")|| `); 
 };
 
 exports.conf = {
