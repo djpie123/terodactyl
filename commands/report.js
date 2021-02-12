ce90e9c32b9ccaba4a4f141d7e1bf4ee40d341e0
djpie123 <shreyashvijay1710@gmail.com>
2021-02-12 14:15:04 +0530

    new

--- commands/report.js
+++ commands/report.js
@@ -1,9 +1,15 @@
+const {MessageEmbed} = require("discord.js");
+
+const {MessageEmbed} = require("discord.js")
 exports.run = (client, message) => { 
     let user = message.author;
     let args = message.content.split(" ").slice(1);
-    message.react("✅ "); 
-    if (args.join(" ") === "@everyone" || args.join(" ") === "@here") return message.channel.send("You ain't making me Ping anyone BOI!"); // Let’s see what’s going on if you want everyone
-   client.users.cache.get("706714852571348993").send(`${user.username}#${user.discriminator} has reprted a bug ||${args.join(" ")}|| `); 
+    message.react("☑️"); 
+    if (args.join(" ") === "@everyone" || args.join(" ") === "@here") return message.channel.send("You ain't making me Ping anyone BOI!"); 
+    let bug = new MessageEmbed()
+    .setTitle(`${user.username}#${user.discriminator} has reprted a bug`)
+    .setDescription(args.join(" "))
+   client.users.cache.get("706714852571348993").send(bug); 
 };
 
 exports.conf = {
