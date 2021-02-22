8fa62c54eadc570154e02699a11236a3ec12076a
djpie123 <shreyashvijay1710@gmail.com>
2021-02-22 13:34:12 +0530

    new

--- commands/nuke.js
+++ commands/nuke.js
@@ -11,7 +11,11 @@ module.exports = {
         let newchannel = await message.channel.clone()
         newchannel.setPosition(message.channel.position)
         await message.channel.delete()
-        await newchannel.send(`nuked ${message.chanel.name} channel because ${reason} . \n https://i.imgur.com/4mJ3qcQ.gif`)
+        const em = new MessageEmbed()
+        .setTitle("BOOOOOM")
+        .setDescription(reason)
+        .setImage("https://i.imgur.com/4mJ3qcQ.gif")
+        await newchannel.send(em)
     }
 }
 module.exports.help = {
