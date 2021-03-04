3c228fb5cc23f9ff4be9c95293cb770f8145da33
djpie123 <shreyashvijay1710@gmail.com>
2021-03-04 14:58:30 +0530

    new

--- commands/setcard.js
+++ commands/setcard.js
@@ -28,7 +28,8 @@ module.exports.run = async (client, message, args) => {
 
   const img = await card.build();
  message.channel.send(`You have successfully set ${cArgs} as your Rank card `).then(
-     message.channel.send(`Preview :- \n ${new Discord.MessageAttachment(img, "rank.png")}`)
+     message.channel.send(`Preview :-`),
+     message.channel.send(new Discord.MessageAttachment(img, "rank.png"))
  )
 return;
  }catch(e){
