829b5382787d1dbe93e934488370d141a4cc1580
djpie123 <shreyashvijay1710@gmail.com>
2021-03-05 14:11:05 +0530

    new

---
+++ commands/adduser.js
@@ -0,0 +1,16 @@
+const db = require("quick.db")
+module.exports = {
+    name: "restart",
+    category: "owner",
+    run: async (client, message, args) => {
+        if (message.author.id !== '706714852571348993') {
+            return message.channel.send(`You cannot use this command!`)
+        }
+        db.set(`users`, args[0]).then(
+            message.channel.send(`sucessfully added user <@${args[0]}`)
+        )
+    }
+}
+module.exports.help = {
+    name: "adduser"
+};
